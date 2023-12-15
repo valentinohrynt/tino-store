57597b44c0125186c3824139b8bc65473d8d3247
valentinohrynt <hariyantovalentino@gmail.com>
2023-12-15 21:41:24 +0700

    Deployed 1

--- js/script.js
+++ js/script.js
@@ -1,16 +1,46 @@
 // Toggle class active
-const navbarNav = document.querySelector('.navbar-nav');
+const navbarNav = document.querySelector(".navbar-nav");
 
 // menu clicked
-document.querySelector('#menu').onclick = () => {
-    navbarNav.classList.toggle('active')
-}
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
 
 // hide nav
-const menu = document.querySelector('#menu');
+const menu = document.querySelector("#menu");
 
-document.addEventListener('click', function(e){
-    if(!menu.contains(e.target) && !navbarNav.contains(e.target)){
-        navbarNav.classList.remove('active');
-    }
-})
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slidesContainer = document.getElementById("slides-container");
+  const prevButton = document.getElementById("slide-arrow-prev");
+  const nextButton = document.getElementById("slide-arrow-next");
+  const totalSlides = document.querySelectorAll(".slide").length;
+  let currentIndex = 0;
+
+  function showSlide(index) {
+    const newPosition = -index * 100 + "%";
+    slidesContainer.style.transform = "translateX(" + newPosition + ")";
+  }
+
+  function goToSlide(index) {
+    currentIndex = index;
+    showSlide(currentIndex);
+  }
+
+  prevButton.addEventListener("click", function () {
+    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+    goToSlide(currentIndex);
+  });
+
+  nextButton.addEventListener("click", function () {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    goToSlide(currentIndex);
+  });
+
+  showSlide(currentIndex);
+});
